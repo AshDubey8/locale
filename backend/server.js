78bf02b2a1d72c89acc7f71d6f84307f7e2be806
Ashuthosh Dubey <AshuthoshMaheshDubey@gmail.com>
2025-09-06 17:47:53 +0530

    add events endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/events', (req, res) => {
+  const events = [
+    {
+      id: '1',
+      name: 'Music Event',
+      date: '2025-09-09',
+      location: 'Uppal Stadium',
+      city: 'Hyderabad'
+    },
+    {
+      id: '2', 
+      name: 'Dance Event',
+      date: '2025-09-10',
+      location: 'Mega Mall',
+      city: 'Bangalore'
+    }
+  ];
+  res.json(events);
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
